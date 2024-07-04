9bde6a4119e9472e7f050001c2bfb48dac0c1d44
Angelo Sanfilippo <124434790+AngeloSan86@users.noreply.github.com>
2024-07-04 15:30:11 +0200

    Add files via upload

---
+++ Selfwork Variabili/main.js
@@ -0,0 +1,19 @@
+let num1 = 10;
+let num2 = 15;
+
+let valore_txt = `Il valore della variabile num1 e'`;
+
+console.log(valore_txt + ` ` + num1);
+console.log(valore_txt + ` ` + num2);
+
+let stringa = `Stringa Causuale`;
+console.log(valore_txt + ` ` + `"` + stringa + `"`);
+
+stringa = `Nuovo valore stringa`;
+console.log(valore_txt + ` ` + `"` + stringa + `"`);
+
+const PIGRECO = 3.14159265359;
+console.log(`il valore di PIGRECO e' ${PIGRECO}`);
+
+PIGRECO = 7;
+console.log(`il valore di PIGRECO e' ${PIGRECO}`);
